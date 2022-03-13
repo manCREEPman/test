ece9ae7b039abd117f313924b7ad3397c0c9cb10
Andrey <sample@example.com>
2022-03-13 18:32:30 +0300

    тест вк

--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from vk_api import vk_api
 
 app = Flask('new1')
 CORS(app)
 
+
+@app.route('/test/vk_test')
+def vk_test():
+    app_id = 8099115
+    code = request.args.get('code')
+    client_secret = 'r6ha68mgEhAB78JFh1zk'
+    vk_session = vk_api.VkApi(app_id=app_id, client_secret=client_secret)
+    redirect_url = 'http://91.132.102.83/test/vk_test'
+    try:
+        vk_session.code_auth(code, redirect_url)
+        vk = vk_session.get_api()
+        vk.wall.post(
+            owner_id=-195146403,
+            message='suck'
+        )
+        return jsonify({'success': 'it is alright'})
+    except vk_api.AuthError as error_msg:
+        return jsonify({'error': error_msg})
+
+
 @app.route('/test')
 def index():
     return jsonify({'data': '^____________^'})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- sample/src/views/Home.vue
+++ sample/src/views/Home.vue
@@ -3,6 +3,8 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <input type="button" value="knopka" @click="getTestData">
+    <div>{{ this.str }}</div>
+    <input type="button" value="vk test" @click="getVkSecretCode">
   </div>
 </template>
 
@@ -25,11 +27,14 @@ export default {
   methods: {
     getTestData() {
       fetch('http://91.132.102.83/test')
-      .then( (response) => {
-        this.str = response.json()
-        console.log(this.str)
-      })
+      .then(response => response.json())
+      .then(data => this.str = data.data)
       .catch(error => console.log(error))
+    },
+
+    getVkSecretCode(){
+      fetch('https://oauth.vk.com/authorize?client_id=8099115&display=popup&redirect_uri=http://91.132.102.83/test/vk_test&scope=wall&response_type=code')
+      .then(response => console.log(response))
     }
   }
 }
