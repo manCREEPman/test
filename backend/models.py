ea2740872b56e5aa9d4dac945f34054736c6fe1e
Andrey <sample@example.com>
2022-03-15 21:43:53 +0300

    Добавил принт traceback в консоль и файл

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Test(db.Model):
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
     def __init__(self, id, name):
--- backend/test.py
+++ backend/test.py
@@ -2,17 +2,23 @@ from json import load
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from models import db, Test
+import traceback
 
 app = Flask('new1')
 CORS(app)
 
-
-f = open('./config.json', 'r')
-db_str = load(f).get('db_connection')
-f.close()
-app.config['SQLALCHEMY_DATABASE_URI'] = db_str
-db.init_app(app)
-    
+try:
+    f = open('./config.json', 'r')
+    db_str = load(f).get('db_connection')
+    f.close()
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_str
+    db.init_app(app)
+except:
+    traceback.print_exc()
+    tb = traceback.format_exc()
+    with open('./log.txt', 'w') as f:
+        f.write(tb)
+        f.close()
 
 @app.route('/api/test_db_api')
 def test_db_api():
